1423f2e8346b5c23a6f7f7005369e6ca12873c62
huangtonghui <535319672@qq.com>
2024-03-16 10:47:53 +0800

    worker

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
       username: "You",
       // auth: 'sess-Vye45UwA1UKHDwpksE76wtGhneBK9Gn6bHccCt9s',
       msgList: [],
-      expirDay: 1,
+      expirDay: 0,
       clickNum: 0,
       cacheList: [],
       historyIndex: "",
